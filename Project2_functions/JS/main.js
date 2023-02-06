04c60036be0068ebdfac542c7ce19ee1a7d685d6
1300Aloce4403 <lskysm@gmail.com>
2023-02-06 20:59:27 +0000

    Assignment submission #2

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function calcArea() {   //Creating the function
+    var w = document.getElementById("width").value;     //Declaring and assigning
+    var h = document.getElementById("height").value;    //the values of the elements
+    var result = w*h;     //Assigning the result of the operation
+    result += " is the area.";      //Concatenating the string to the result
+    alert(result);      //Showing the result as an alert
+}
