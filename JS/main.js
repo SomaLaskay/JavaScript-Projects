94638f466893af19a06a08f3a50d5bfcf621ad9f
1300Aloce4403 <lskysm@gmail.com>
2023-02-06 01:17:20 +0000

    Assignment submission 1

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,24 @@
-window.alert();
-document.write('Hello Man');
+//Declare a variable
+var stringVar = "Something"; 
+window.alert(stringVar);
+
+//Escaping character
+var A = "This is a string: \"Something is just nothing\"!";
+document.write(A+" This part is just got concatenated to the first part.<br>");
+
+//Concatenation
+var concatString = "\"This part is being\" "+"\"concatenated to this part\"<br>";
+document.write(concatString);
+
+//Multiple variables
+var myName = "Soma", myAge = "39", myFavColor = "violet";
+document.write(myFavColor+" ");
+
+//Methods
+var myName = myName.fontcolor("red"), myAge = myAge.fontcolor("green"), myFavColor = myFavColor.fontcolor("violet");
+document.write(myName+" "+myAge+" "+myFavColor);
+
+//Expression
+var calc = 5+5;
+document.write("<br>"+calc);
+
