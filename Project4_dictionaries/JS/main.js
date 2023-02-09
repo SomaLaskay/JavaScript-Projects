2a46300e141b0027380b9c72cd45a407215a2032
1300Aloce4403 <lskysm@gmail.com>
2023-02-09 17:27:30 +0000

    Project4

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,9 @@
+//Basic dictionary
+function dictRead() {
+    var dicto {
+        Job: "Project 1",
+        Quantity: 10,
+        Type: "basic"
+    };
+    document.getElementById("result").innerHTML = dicto.Job;
+}
